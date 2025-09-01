b6b70fa2f1ff05cb7cbaa4c7a1bf48f30490092a
adr!an <111903096+adrianvrj@users.noreply.github.com>
2025-08-31 20:46:13 -0600

    #1 small fixes

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="bg-dark-bg min-h-screen text-white snap-y snap-mandatory">
+    <div className="bg-dark-bg min-h-screen text-white overflow-y-scroll snap-y snap-mandatory h-screen">
       <ScrollAnimations />
       <SectionNavigation />
 
